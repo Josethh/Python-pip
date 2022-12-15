afab1f702fe2dea9f757903cbf44e00e075d181c
Joseth <jossetth@outlook.com>
2022-12-15 06:33:25 -0500

    Python in Docker Containers

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,20 @@
 import store 
+from fastapi import FastAPI 
+from fastapi.responses import HTMLResponse 
 
+app = FastAPI()
+
+@app.get('/') # Suelen llamarlo Recurso ó endpoint
+def get_list():
+    return [1, 2, 3]
+
+@app.get('/contact', response_class= HTMLResponse) # Suelen llamarlo Recurso ó endpoint
+def get_list():
+    return """
+        <h1>Hola soy una página</h1>
+        <p>Soy un parrafo</p>
+
+    """
 
 def run():
     store.get_categories() 
